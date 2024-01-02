edd5819c1aea8e87622c46448d73a0016d4c3739
lvheng <1834805770@qq.com>
2024-01-02 21:28:13 +0800

    feat: update vite to 5.0

--- example/src/App.vue
+++ example/src/App.vue
@@ -1,5 +1,7 @@
 <template>
-  <div>
+  <div class="demo">
+    <Icon name="community" />
+    <Icon name="support" />
     <Icon name="tooling" />
   </div>
 </template>
@@ -9,30 +11,8 @@ import Icon from './components/Icon.vue'
 </script>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.demo {
+  display: flex;
+  gap: 12px;
 }
 </style>
